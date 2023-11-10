6a86c11186de207b1f0d46858ca47f26ff9d55c0
aditya paluskar <adityapaluskar@gmail.com>
2023-11-10 12:05:10 +0530

    Added search and filter features

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './Product'
 
 export default function AllProducts() {
@@ -24,24 +24,83 @@ export default function AllProducts() {
     {id: 19, title: "Mens Hoodie Black Print",  src: "https://images.unsplash.com/photo-1512401671639-e0ef01671b17?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGhvb2RpZXN8ZW58MHx8MHx8fDA%3D", price: 600},
     {id: 20, title: "Mens Hoodie Blue Print",  src: "https://images.unsplash.com/photo-1608055599516-99caeb08ab3c?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fGhvb2RpZXN8ZW58MHx8MHx8fDA%3D", price: 550}
   ];
+  const [all, setAll] = useState(prods);
+  const [isSrch, setVal] = useState(false)
+  const [srch, setSrch] = useState('');
+
+
+  const handleSearch = (name) => {
+    if (!isSrch) {
+      if (srch.length < 1) {
+        alert('Please enter something to search...')
+      } else {
+        setVal(true);
+        const new_items = all.filter(el => el.title.includes(srch))
+        if (new_items.length > 0) {
+          setAll(new_items);
+        } else if (new_items.length < 1) {
+          setAll(prods)
+        }
+      }
+    } else {
+      setVal(false);
+      setAll(prods);
+      setSrch('')
+    }
+  }
+
+  const handleFilter = (e) => {
+    if (e.target.checked) {
+      setAll(all.filter(el => el.title.includes(e.target.name)));
+    } else {
+      setAll(prods);
+    }
+  }
 
   return (
-    <div className='flex justify-center items-center'>
-      <div className='w-1/4 h-full'>
-        <h1>Left Section</h1>
+    <div className='flex'>
+      <div className='flex flex-col w-1/4 border-r border-gray-300 flex justify-start items-start p-4'>
+        <h1 className='text-2xl'>Filters</h1>
+        <div className='flex flex-col mt-2'>
+          <div className='flex justify-start items-center'>
+            <input id='checkbox1' type='checkbox' name='shirt' className='mr-1' onChange={handleFilter}/>
+            <p className='text-sm'>Shirt</p>
+          </div>
+          <div className='flex justify-start items-center'>
+            <input id='checkbox2' type='checkbox' name='t-shirt' className='mr-1' onChange={handleFilter}/>
+            <p className='text-sm'>T-Shirt</p>
+          </div>
+          <div className='flex justify-start items-center'>
+            <input id='checkbox3' type='checkbox' name='jeans' className='mr-1' onChange={handleFilter}/>
+            <p className='text-sm'>Jeans</p>
+          </div>
+          <div className='flex justify-start items-center'>
+            <input id='checkbox4' type='checkbox' name='Hoodie' className='mr-1' onChange={handleFilter}/>
+            <p className='text-sm'>Hoodies</p>
+          </div>
+          <div className='flex justify-start items-center'>
+            <input id='checkbox5' type='checkbox' name='shorts' className='mr-1' onChange={handleFilter}/>
+            <p className='text-sm'>Shorts</p>
+          </div>
+        </div>
       </div>
-      <div className='w-full h-full p-4 flex flex-col border-l border-gray-300'>
+      <div className='w-full h-full p-4 flex flex-col'>
         <div className='w-full flex'>
-          <input type='text' className='outline-none border border-gray-300 rounded-l-md w-full px-2 py-1' />
-          <button className='rounded-r-md bg-black px-3 py-1 text-white'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="font-bold w-4 h-4 text-white">
+          <input value={srch} onChange={e => setSrch(e.target.value)} type='text' className='outline-none border border-gray-300 rounded-l-md w-full px-2 py-1' />
+          <button onClick={handleSearch} className='rounded-r-md bg-black px-3 py-1 text-white'>
+            {
+              isSrch ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="font-bold w-4 h-4 text-white">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+              : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="font-bold w-4 h-4 text-white">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
+            }
           </button>
         </div>
         <div className='grid lg:grid-cols-4 md:grid-cols-3 gap-3 mt-4'>
           {
-            prods.map(el => <Product title={el.title} price={el.price} src={el.src} key={el.id} prod={el} />)
+            all.map(el => <Product title={el.title} price={el.price} src={el.src} key={el.id} prod={el} />)
           }
         </div>
       </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -4,9 +4,8 @@ import { addToCart } from '../features/cartSlice/cartSlice';
 
 export default function Product({ src, title, price, prod }) {
   const disp = useDispatch();
-  const add = (prod) => {
-    disp(addToCart(prod));
-  }
+  const add = prod => disp(addToCart(prod));
+
   return (
     <div className='flex flex-col border border-gray-300 rounded-md p-4'>
       <div className='h-[17rem] w-full'>
@@ -15,11 +14,11 @@ export default function Product({ src, title, price, prod }) {
       <div className='flex justify-between items-center mt-3'>
         <div className='flex flex-col justify-center items-start'>
           <h1 className='font-bold text-sm tracking-tighter'>{title}</h1>
-          <p className='text-xs'>${price}</p>
+          <p className='text-xs'>Rs.{price}</p>
         </div>
         <div className='flex'>
           <button className='bg-black text-white text-xs rounded-lg p-2 py-1 mx-1'>view</button>
-          <button onClick={() => {add(prod)}} className='bg-black text-white text-xs rounded-lg px-1 py-1 ml-1'>add to cart</button>
+          <button onClick={() => add(prod)} className='bg-black text-white text-xs rounded-lg px-1 py-1 ml-1'>add to cart</button>
         </div>
       </div>
     </div>
